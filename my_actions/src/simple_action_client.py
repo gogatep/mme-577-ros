1ff78855403bdb47e724cb13300b6872a96f7026
gogatep <64620214+gogatep@users.noreply.github.com>
2020-05-10 23:39:54 -0700

    Update simple_action_client.py

--- my_actions/src/simple_action_client.py
+++ my_actions/src/simple_action_client.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python
-
+##importing modules
 import rospy
 
 import actionlib
 from my_actions.msg import TimerAction, TimerGoal, TimerResult
 
-
+#setup
 rospy.init_node('timer_action_client')
 client = actionlib.SimpleActionClient('timer', TimerAction)
-client.wait_for_server()
-goal = TimerGoal()
-goal.time_to_wait = rospy.Duration.from_sec(5.0)
+client.wait_for_server()#wait
+goal = TimerGoal()#set goal object
+goal.time_to_wait = rospy.Duration.from_sec(5.0)#duration
 client.send_goal(goal)
 client.wait_for_result()
-print('Time elapsed: %f' % (client.get_result().time_elapsed.to_sec()))
+print('Time elapsed: %f' % (client.get_result().time_elapsed.to_sec()))#print only once since no need to update.
