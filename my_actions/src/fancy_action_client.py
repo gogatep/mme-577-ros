e8f8d78f649984be7f0847482450eaf8f5822b7c
gogatep <64620214+gogatep@users.noreply.github.com>
2020-05-10 23:47:27 -0700

    Update fancy_action_client.py

--- my_actions/src/fancy_action_client.py
+++ my_actions/src/fancy_action_client.py
@@ -29,6 +29,7 @@ client.wait_for_result()# wait for action server to finish
 ##printing
 print('[Result] State: %d' % (client.get_state()))
 print('[Result] Status: %s' % (client.get_goal_status_text()))
-print('[Result] Time elapsed: %f' %
+if client.get_result():
+    print('[Result] Time elapsed: %f' %
       (client.get_result().time_elapsed.to_sec()))
-print('[Result] Updates sent: %d' % (client.get_result().updates_sent))
+    print('[Result] Updates sent: %d' % (client.get_result().updates_sent))
