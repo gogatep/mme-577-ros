95c658f9360421855d90ad72508c6af1b27da5f7
gogatep <64620214+gogatep@users.noreply.github.com>
2020-05-10 23:35:29 -0700

    Update fancy_action_client.py

--- my_actions/src/fancy_action_client.py
+++ my_actions/src/fancy_action_client.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+## import modules
 import rospy
 
 import time
@@ -7,25 +7,26 @@ import actionlib
 from my_actions.msg import TimerAction, TimerGoal
 
 
-def feedback_cb(feedback):
+def feedback_cb(feedback):# feedback callback function
     print('[Feedback] Time elapsed: %f' % (feedback.time_elapsed.to_sec()))
     print('[Feedback] Time remaining: %f' % (feedback.time_remaining.to_sec()))
 
-
-rospy.init_node('timer_action_client')
-client = actionlib.SimpleActionClient('timer', TimerAction)
-client.wait_for_server()
-goal = TimerGoal()
-goal.time_to_wait = rospy.Duration.from_sec(5.0)
+#setup
+rospy.init_node('timer_action_client')# initialize node
+client = actionlib.SimpleActionClient('timer', TimerAction)# register client
+client.wait_for_server()# wait for action server
+goal = TimerGoal()# create goal
+goal.time_to_wait = rospy.Duration.from_sec(5.0)# set field
 # Uncomment this line to test server-side abort:
 #goal.time_to_wait = rospy.Duration.from_sec(500.0)
 
-client.send_goal(goal, feedback_cb=feedback_cb)
+client.send_goal(goal, feedback_cb=feedback_cb)# send goal
 # Uncomment these lines to test goal preemption:
 #time.sleep(3.0)
 #client.cancel_goal()
 
-client.wait_for_result()
+client.wait_for_result()# wait for action server to finish
+##printing
 print('[Result] State: %d' % (client.get_state()))
 print('[Result] Status: %s' % (client.get_goal_status_text()))
 print('[Result] Time elapsed: %f' %
