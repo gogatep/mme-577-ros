fa1e2f4bce1ab4edc1a2490229e1f9da0fe8bf42
gogatep <64620214+gogatep@users.noreply.github.com>
2020-05-10 23:38:10 -0700

    Update fancy_action_server.py

--- my_actions/src/fancy_action_server.py
+++ my_actions/src/fancy_action_server.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+## importing modules
 import rospy
 
 import time
@@ -7,17 +7,17 @@ import actionlib
 from my_actions.msg import TimerAction, TimerResult, TimerFeedback
 
 
-def do_timer(goal):
-    start_time = time.time()
+def do_timer(goal):# action function
+    start_time = time.time()#needed to tell how much time has elapsed
     update_count = 0
-    if goal.time_to_wait.to_sec() > 60.0:
+    if goal.time_to_wait.to_sec() > 60.0:# check req duration
         result = TimerResult()
         result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
         result.updates_sent = update_count
         server.set_aborted(result, "Timer aborted due to too-long wait")
-        return
-    while (time.time() - start_time) < goal.time_to_wait.to_sec():
-        if server.is_preempt_requested():
+        return# too long of a requested wait
+    while (time.time() - start_time) < goal.time_to_wait.to_sec():# waiting to meet goal duration
+        if server.is_preempt_requested():# check preemption
             result = TimerResult()
             result.time_elapsed = rospy.Duration.from_sec(
                     time.time() - start_time)
@@ -29,15 +29,15 @@ def do_timer(goal):
                 time.time() - start_time)
         feedback.time_remaining = goal.time_to_wait - feedback.time_elapsed
         server.publish_feedback(feedback)
-        update_count += 1
+        update_count += 1#to show at the end 
         time.sleep(1.0)
     result = TimerResult()
     result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
     result.updates_sent = update_count
     server.set_succeeded(result, "Timer completed successfully")
 
-
-rospy.init_node('timer_action_server')
+#setup
+rospy.init_node('timer_action_server')# initialize node
 server = actionlib.SimpleActionServer('timer', TimerAction, do_timer, False)
 server.start()
-rospy.spin()
+rospy.spin()#keep node alive
